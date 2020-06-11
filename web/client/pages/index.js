f8e688726107beb6d0545bfd4b1b4b0531abcff0
Mohameth Seck <mohamethseck34@gmail.com>
2020-06-11 09:20:58 -0400

    home page text

--- web/client/pages/index.js
+++ web/client/pages/index.js
@@ -1,5 +1,9 @@
 import Layout from '../components/Layout';
 
-const Home = () => <Layout>VRS videos will appear here</Layout>;
+const Home = () => <Layout>
+VRS videos will appear here
+
+<p>For more info check out the <a href="https://medium.com/@mohamethseck/vrs-beta-2020-aedbea9b446c">Medium blog post</a></p>
+</Layout>;
 
 export default Home;
