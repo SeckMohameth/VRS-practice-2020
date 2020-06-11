c9025ae5df4e4aa10213471dd4e909f5369a4d0d
Mohameth Seck <mohamethseck34@gmail.com>
2020-06-11 09:27:49 -0400

    added more content to home page

--- web/client/pages/index.js
+++ web/client/pages/index.js
@@ -1,9 +1,14 @@
 import Layout from '../components/Layout';
 
 const Home = () => <Layout>
-VRS videos will appear here
+<h1>The Billboard Hot 100, but for videos</h1>
+
+VRS videos will appear here. A search bar will be at the top to search for locations and users.
 
 <p>For more info check out the <a href="https://medium.com/@mohamethseck/vrs-beta-2020-aedbea9b446c">Medium blog post</a></p>
+
+<p>Currently working on the iOS app</p>
+
 </Layout>;
 
 export default Home;
